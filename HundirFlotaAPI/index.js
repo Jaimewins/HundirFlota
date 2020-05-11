a76f7b985ca517046aba88fc4a21c94b34ca0f3e
ShuviDola <enriqueojedaroig1993@gmail.com>
2020-05-11 20:49:44 +0200

    UPDATE: Generador de barcos

--- HundirFlotaAPI/index.js
+++ HundirFlotaAPI/index.js
@@ -4,154 +4,261 @@ var app        = express();                 // definimos la app usando express
 var bodyParser = require('body-parser'); //
 
 var UsersModel  = require('./models/userModel');
-/*
-UsersModel.create(
-    {
-    "name": "Jandu",
-    "email": "cachoperro@yopmail.com"
-    }
-);
-
-UsersModel.findOne({
-    "email": "cachoperro@yopmail.com"},
-    function (err, doc) {
-        if(err){
-            console.log(err);
-            return;
-        }
-
-        console.log(doc.name);
-        
-        doc.name = "Pepito";
-        doc.save(); 
-
-        console.log(doc.name);
-    }
-);
-
-UsersModel.find({
-    "email": "cachoperro@yopmail.com"},
-    function (err, docs) {
-        if(err){
-            console.log(err);
-            return;
-        }
-
-        docs.forEach(doc => {
-            console.log(doc.name + " "+ doc.email+ " "+ doc._id);            
-        });
-    }
-);
-*/
-// configuramos la app para que use bodyParser(), esto nos dejara usar la informacion de los POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-var port = process.env.PORT || 8080;        // seteamos el puerto
-
-var router = express.Router();   //Creamos el router de express
-
-// Seteamos la ruta principal
-router.get('/', function(req, res) {
-    res.json({ message: 'Hooolaa :)'});
-});
-
-
-var gameonline = express.Router();
-
-gameonline.get('/', function(req, res) {
-    res.json({ message: 'Api GameOnline :)'});
-});
-
-
-gameonline.post('/user', function(req,res){
-        console.log("paso por user post");
-        console.log(req.body);
-        if(req.body.name != null && req.body.email != null){
-            UsersModel.create(
-                {
-                "name": req.body.name,
-                "email": req.body.email
-                }, 
-                function(err){
-                    if(err){ res.end(err)}
-                    else{ res.end("Usuario insertado correctamente")}
-                }
-            );
-        }else{
-            res.end('ERROR');
-        }
-    }
-);
 
 //======================================================
-var ahorcado = express.Router();
+var baquitos = express.Router();
 
-function codifica(entrada){
-    var salida="";
 
-    for (var i = 0; i< entrada.length; i++){
-        salida+="*";
+// VARIABLES
+var matriz = new Array(2);
+
+var barcoPeque = [];
+var barcoMediano = [];
+var barcoGrande = [];
+
+var indexBarco = 0;
+var tablaDirecciones = [0, 1, -1];
+var barcoColocar = [] 
+
+var posInitX = 0;
+var posInitY = 0;
+
+var direccionBarco = []
+
+var listaDisparosIA = []
+// METODOS
+
+var vidaBarcoPequeno = 2;
+var vidaBarcoMediano = 4;
+var vidaBarcoGrande = 6;
+
+function parsearDireccio(grados){
+
+    if(grados == 0){
+
+        return [0, -1];
+    } else if(grados == 90){
+
+        return [1, 0];
+    } else if(grados == 180){
+
+        return [0, 1];
+    } else {
+
+        return [-1, 0];
     }
-
-    return salida;
 }
 
-function existecaracter(letra, palabra){
-    return palabra.indexOf(letra)>=0;
+function ColocarBarcos(identificador, posInicial, direccion, tamanoBarco){
+
+    arrayDireccion =  parsearDireccio(direccion);
+    for (let i = 0; i < tamanoBarco; i++) {
+
+        matriz[posInicial[0+(arrayDireccion[0]*i)]][posInicial[1]+(arrayDireccion[1]*i)] = identificador;
+    }
 }
 
-function procesacaractercifrado(letra){
-    var actual="";
-    for(var i=0; i< palabraActual.length;i++){
-        if(palabraActual.charAt(i) == letra){
-            actual+=letra;
-        }else{
-            actual+=palabraActualCifrada.charAt(i);
+
+function ComprobarBarcos(x, y, tipoBarco){
+    
+    var direccionX = x;
+    var direccionY = y;
+    direccionBarco = [x, y]
+
+    barcoColocar.push(matrix[posInitX][posInitY]);
+
+    for (let i = 1; i <= tipoBarco; i++) {
+
+        if(posInitX+(i*tablaDirecciones[direccionX]) < 10 && posInitY+(i*tablaDirecciones[direccionY]) < 10 ){
+
+            if(matrix[posInitX+(i*tablaDirecciones[direccionX])][posInitY+(i*tablaDirecciones[direccionY])] != "A"){
+
+                if(direccionX == 1 && direccionY == 0){
+                    
+                    ComprobarBarcos(0, 1, tipoBarco);
+                } else if(direccionX == 0 && direccionY == 1){
+                    
+                    ComprobarBarcos(2, 0, tipoBarco);
+                } else if(direccionX == 2 && direccionY == 0){
+                    
+                    ComprobarBarcos(0, 2, tipoBarco);
+                } else{
+                    
+                    return false;
+                } 
+            }
+        } else {
+
+            return false;
+        }
+    } 
+
+    return true;
+}
+
+function ColocarBarcosIA(tipoBarco){
+
+    do{
+
+        posInitX = Math.floor(Math.random() * 10);
+        posInitY = Math.floor(Math.random() * 10);
+    } while(!matrix[posInitX][posInitY] == "A");
+ 
+    switch (indexBarco) {
+        case 0:
+            
+            if(ComprobarBarcos(1, 0, tipoBarco)){
+
+                indexBarco = 1;
+                barcoPeque = [posInitX, posInitY];
+                ColocarBarcos("PIA", barcoPeque, direccionBarco, tipoBarco)
+                barcoColocar = [];
+            } else {
+
+                ColocarBarcosIA(4);
+            }            
+            break;
+        case 1:
+
+            if(ComprobarBarcos(1, 0, tipoBarco)){
+
+                indexBarco = 2;
+                barcoMediano = [posInitX, posInitY];
+                ColocarBarcos("MIA", barcoMediano, direccionBarco, tipoBarco)
+                barcoColocar = [];
+            } else {
+
+                ColocarBarcosIA(6);
+            }  
+            break;
+        case 2:
+    
+            if(ComprobarBarcos(1, 0, tipoBarco)){
+
+                indexBarco = 2;
+                barcoGrande = [posInitX, posInitY];
+                ColocarBarcos("GIA", barcoGrande, direccionBarco, tipoBarco)
+                barcoColocar = [];
+            } else {
+
+                ColocarBarcosIA(6);
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+
+function disparaPlayer(coordenadaPlayer){
+
+    if(matriz[coordenadaPlayer[0]][coordenadaPlayer[1]] == "PIA"){
+
+        vidaBarcoPequeno--;
+        return true;
+    } else if(matriz[coordenadaPlayer[0]][coordenadaPlayer[1]] == "MIA"){
+
+        vidaBarcoMediano--;
+        return true;
+    } else if(matriz[coordenadaPlayer[0]][coordenadaPlayer[1]] == "GIA"){
+
+        vidaBarcoGrande--;
+        return true;
+    } else {
+        
+        return false;
+    }
+}
+
+function disparaIA(){
+
+    var coordDispIAX;
+    var coordDispIAY;
+
+    coordDispIAX = Math.floor(Math.random() * 10);
+    coordDispIAY = Math.floor(Math.random() * 10);
+
+    array.forEach(listaDisparosIA => {
+        
+        if(coordDispIAX == array[0] && coordDispIAY == array[1]){
+
+            disparaIA()
+        }
+    });
+
+    listaDisparosIA.push([coordDispIAX][coordDispIAY]);
+
+    if(listaDisparosIA.lastIndexOf() == "P"){
+
+    } else if(listaDisparosIA.lastIndexOf() == "M"){
+
+    } else if(listaDisparosIA.lastIndexOf() == "G"){
+
+    } else {
+
+    }
+}
+
+//LLAMADAS
+
+baquitos.get('/cargamatriz', function(req, res) {
+
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            
+            matriz[i][j] = "A";
         }
     }
-    palabraActualCifrada = actual;
-}
 
-var palabras = [ 'PEPITO', 'JUANITO', 'MARQUITO', 'BOLLICAO'];
-var indicepalabra = 0;
-
-var palabraActual = palabras[indicepalabra];
-var palabraActualCifrada = codifica(palabraActual);
-
-
-ahorcado.get('/currentword', function(req, res) {
     res.json({ "word": palabraActualCifrada});
 });
 
 
-ahorcado.post('/playword', function(req, res) {
-    if(req.body.word){
-        if(palabraActual == req.body.word){
-            //cambiar de palabra
-            indicepalabra++;
-            indicepalabra%=palabras.length;  //-> indicepalabra = indicepalabra % palabras.length;
-            palabraActual = palabras[indicepalabra];
-            palabraActualCifrada = codifica(palabraActual);
-            res.json({ "status": "WIN"});    
-        }else{
-            res.json({ "status": "NOOK"});
+baquitos.get('/colocarbarcos', function(req, res) {
+    if(req.body.barcos){
+        
+        // desfragmetar json barcos
+
+        ColocarBarcos(identificador, posInicial, direccion, tamanoBarco);
+        ColocarBarcosIA(2);
+    }else{
+        res.json({ "status": "ERROR"});
+    }
+});
+
+
+baquitos.post('/dispararplayer', function(req, res) {
+    if(req.body.coordenadaPlayer){
+        
+        if(disparaPlayer(coordenadaPlayer)){
+
+            if(vidaBarcoPequeno <= 0){
+
+                res.json({ "status": "ERROR", "status": "ERROR"});
+            } else if(vidaBarcoMediano <= 0){
+
+                res.json({ "status": "ERROR", "status": "ERROR"});
+            } else if(vidaBarcoGrande <= 0){
+
+                res.json({ "status": "ERROR", "status": "ERROR"});
+            } else {
+
+                res.json({ "status": "ERROR", "status": "ERROR"});
+            }
+
+        } else {
+
+            res.json({ "status": "ERROR"});
         }
     }else{
         res.json({ "status": "ERROR"});
     }
 });
 
-ahorcado.post('/playletter', function(req, res) {
-    console.log(req.body.letter);
-    if(req.body.letter && req.body.letter.length==1){
-        if(existecaracter(req.body.letter, palabraActual)){
-            //procesar palabraactualcifrada
-            procesacaractercifrado(req.body.letter);
-            res.json({ "status": "OK", "word": palabraActualCifrada});
-        }else{
-            res.json({ "status": "NOOK", "word": palabraActualCifrada});
-        }
+baquitos.post('/dispararia', function(req, res) {
+    if(req.body.coordenadaPlayer){
+        
+        disparaIA()
     }else{
         res.json({ "status": "ERROR"});
     }
@@ -159,9 +266,7 @@ ahorcado.post('/playletter', function(req, res) {
 
 
 // Le decimos a la aplicación que utilize las rutas que agregamos
-app.use('/api', router);
-app.use('/gameonline', gameonline);
-app.use('/ahorcado', ahorcado);
+app.use('/baquitos', baquitos);
 
 // Iniciamos el servidor
 app.listen(port);
